d0ea4d3b211210baeb523b14180a1716c10b56a0
Alexander <caaalex54@gmail.com>
2025-09-18 13:20:24 +0200

    Add files via upload

---
+++ 3.1 Summan av två tal/Program.cs
@@ -0,0 +1,18 @@
+﻿var list = new List<double>();
+
+while (true)
+{
+    Console.Write("What is your next number? (Type -1 to exit) : ");
+    double input = 0;
+    while (!double.TryParse(Console.ReadLine(), out input))
+        Console.WriteLine("You had one job. What is your next number? (Type -1 to exit) ");
+
+    if (input == -1)
+        break;
+
+    list.Add(input);
+}
+
+Console.WriteLine("First number: {0}", list.Max());
+Console.WriteLine("Second number: {0}", list.Min());
+Console.WriteLine("Total: {0}", list.Sum());
