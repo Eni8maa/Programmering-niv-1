d61ab066f0de2cd63b91185594c901914f4082e6
Alexander <caaalex54@gmail.com>
2025-09-18 13:28:45 +0200

    Add files via upload

---
+++ Temp/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Är det fint väder?"); //Text del för användaren
+Console.Write("Välj ja eller nej: "); //Text del för användaren
+string UserValue = Console.ReadLine(); //Skapar en variabel för att kunna använda användarens inmatning
+
+if (UserValue == "ja") //Om användarens inmatning är "ja"
+{
+    Console.WriteLine("Då går vi på picknick!"); //Skrivs ut om användarens inmatning är "ja"
+}
+else if (UserValue == "nej") //Om användarens inmatning är "nej"
+{
+    Console.WriteLine("Då stannnar vi inne."); //Skrivs ut om användarens inmatning är "nej"
+}
+else //Om användarens inmatning inte är "ja" eller "nej"
+{
+    Console.WriteLine("Du måste svara ja eller nej!"); //Skrivs ut om användarens inmatning inte är "ja" eller "nej"
+}
+
+Console.ReadLine(); 
---
+++ Variables/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Varibles
+    {
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            int x;
+            int y;
+
+            x= 7;
+            y=x+3;
+
+            Console.WriteLine(y);
+            Console.ReadLine();
+            */
+            Console.WriteLine("What is your first name?");
+            Console.Write("Type your first name:");
+            string MyFirstName;
+            MyFirstName = Console.ReadLine();
+
+            //string MyLastName;
+            //Console.Write("Type your last name:");
+            //MyLastName = Console.ReadLine();
+
+            Console.Write("Type your last name:");
+            string MyLastName = Console.ReadLine();
+
+            Console.WriteLine("Hello," + MyFirstName + " " + MyLastName);
+            Console.ReadLine();
+        }
+    }
+}
